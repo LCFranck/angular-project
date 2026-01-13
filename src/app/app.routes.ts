1b965e50d0828f1d05c58971fa378e4157eeb637
LCFranck <cassandra.franck@gmail.com>
2026-01-13 20:00:35 +0200

    fixed footer and added details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-//import { CraftDetailComponent } from './craft-detail/craft-detail.component';
+import { CraftDetailComponent } from './craft-detail/craft-detail.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'details', component: CraftDetailComponent }
  
 ];
